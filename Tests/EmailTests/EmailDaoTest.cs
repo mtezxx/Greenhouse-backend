3cffb7146a35e526b7eaade5327436d7efd9b6d2
mtezxx <mkukucko@gmail.com>
2024-05-17 11:29:48 +0200

    Updated tests

--- Tests/EmailTests/EmailDaoTest.cs
+++ Tests/EmailTests/EmailDaoTest.cs
@@ -92,12 +92,4 @@ public class EmailDaoTest : DbTestBase
     {
         await Assert.ThrowsAsync<Exception>(() => _emailDao.GetAsync());
     }
-    
-    [Fact]
-    public async Task CreateAsync_ThrowsArgumentException_WhenEmailIsEmpty()
-    {
-        var notificationEmail = new EmailNotification { Email = string.Empty };
-
-        await Assert.ThrowsAsync<ArgumentException>(() => _emailDao.CreateAsync(notificationEmail));
-    }
 }
